1a2594c620c4a509daeec0e40f935a110e463c66
unknown <patticus.tv@gmail.com>
2021-12-22 01:58:50 +1100

    completed layout for inserting JS tomorrow

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,6 @@ var currentTime = document.getElementById('currentDay');
 
 // Function for obtaining current time using jQuary moment() function
 var timerInterval = setInterval(function () {
-    var actualTime = moment().format("MMMM Do, YYYY");
+    var actualTime = moment().format("dddd Do MMMM , YYYY");
     currentTime.textContent = actualTime
 }, 1000)
